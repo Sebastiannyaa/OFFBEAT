0d7c71d5ef319badde4c82ae89c1e0156713c474
Sebastiannyaa <main.offbeatapp@gmail.com>
2026-04-19 13:48:18 +0200

    Reapply "0.0.1 Prerelease. test build"
    This reverts commit 40601e2f82db941002fe3a3c8147e35b2a892924.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QIcon>
+#include <QQmlContext>
+#include <QtQml>
+#include "Backend.h"
+
+int main(int argc, char *argv[])
+{
+#if defined(Q_OS_WIN) && QT_VERSION_CHECK(5, 6, 0) <= QT_VERSION && QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    app.setWindowIcon(QIcon(":/qt/qml/offbeat/images/OFFBEAT_Logo1.png"));
+
+
+    QQmlApplicationEngine engine;
+
+    Backend backend;
+    qmlRegisterSingletonInstance("CustomClass", 1, 0, "Backend", &backend);
+    // qml: import CustomClass 1.0
+
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/offbeat/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
---
+++ main.qml
@@ -0,0 +1,37 @@
+import QtQuick 2.9
+import QtQuick.Window 2.2
+
+Window {
+    id: rootid
+    visible: true
+    width: 600
+    height: 350
+    color: "black"
+
+    title: "Offbeat"
+
+    flags: Qt.Window | Qt.FramelessWindowHint
+
+    
+
+    
+    
+    Loader {
+        id: mainloader
+        anchors.fill: parent
+        source: "BootPage.qml"
+
+        onLoaded: {
+            item.send = function(message,numx,numy) {
+                mainloader.source = message
+                
+                rootid.width = numx
+                rootid.height = numy
+                
+                rootid.x = (Screen.width - rootid.width) / 2
+                rootid.y = (Screen.height - rootid.height) / 2
+            }
+        }
+    }
+    
+}
